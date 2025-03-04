c3e9968a47c6f2afa6f632733be443d333de72a1
vinodh1988 <k.c.vinodh@hotmail.com>
2025-03-04 15:11:48 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactsComponent},	
+  {path:'projects',component:ProjectsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
