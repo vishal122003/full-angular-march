4790f772617418a05b65adf4ae412c71aac5c207
vinodh1988 <k.c.vinodh@hotmail.com>
2025-03-04 16:30:28 +0530

    model

---
+++ src/app/data/info.ts
@@ -0,0 +1,27 @@
+import { contact } from '../model/contact';
+const contacts:contact[] = [
+    {
+    name: "Chennai Development Center",
+    address: "No. 5/639, Old Mahabalipuram Road,",
+    city: "Chennai",
+    state: "Tamil Nadu",
+    phone: "044-2450-9530",
+    image: "chennai.jpg",
+    },
+    {
+    name: "Mumbai Corporate Office",
+    address: "Unit No. 701, 7th Floor, B-Wing,",
+    city: "Mumbai",
+    state: "Maharashtra",
+    phone: "022-6645-1234",
+    image: "mumbai.jpg",
+    },
+    {
+        name:"Hyderabad Development Center",
+        address:"Plot No. 32, Hitech City Main Road",
+        city:"Hyderabad",
+        state:"Telangana",
+        phone:"040-6789-3456",
+        image:"hyderabad.jpg",
+    }
+]
---
+++ src/app/model/contact.ts
@@ -0,0 +1,8 @@
+export interface contact {
+    name: string;
+    address: string;
+    city: string;
+    state: string;
+    phone: string;
+    image: string;
+}
